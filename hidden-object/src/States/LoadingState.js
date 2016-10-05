a7521caa591022e7edaf58545bb9d9d109b1b85b
Paul van Dam <p.vandam@coolblue.nl>
2016-10-05 11:22:12 +0200

    Nieuwe platjes

--- hidden-object/src/States/LoadingState.js
+++ hidden-object/src/States/LoadingState.js
@@ -9,49 +9,25 @@
 *
 */
 
-/**
-* Since we want to use the custom Kiwi.JS loader with the bobing kiwi/html5 logo and everything. We need to extend the KiwiLoadingScreen State.
-* The KiwiLoadingScreen State is an extentsion of a normal State but it has some custom code to handle the loading/bobbing/fading of all the items, so if you override a method (like the preload) for example just make sure you call the super method.
-*
-* The parameters we are passing into this method are as ordered.
-* 1 - name {String} Name of this state.
-* 2 - stateToSwitch {String} Name of the state to switch to AFTER all the assets have loaded. Note: The state you want to switch to should already have been added to the game.
-* 3 - dimensions {Object} A Object containing the width/height that the game is to be. For example {width: 1024, height: 768}
-* 4 - subfolder {String} The folder that the loading graphics are located at.
-*/
-
 var LoadingState = new Kiwi.State( "LoadingState" );
 
-
-//var LoadingState = new KiwiLoadingScreen('LoadingState',  'PlayState', {width:1334, height: 750}, 'assets/img/loading/');
-
-/**
-* This preload method is responsible for preloading all your in game assets.
-* Since we are extending the KiwiLoadingScreen we need to make sure we call the preload method on the KiwiLoadingScreen.
-* @method preload
-* @private
-*/
 LoadingState.preload = function () {
 
-    //Make sure to call the super at the top.
-    //Otherwise the loading graphics will load last, and that defies the whole point in loading them.
-    //KiwiLoadingScreen.prototype.preload.call(this);
+    this.game.stage.resize(1334, 750);
 
-    this.addImage('bg', 'assets/img/bg.jpg');
-    this.addImage('hidden_1', 'assets/img/hidden_1.png');
-    this.addImage('hidden_2', 'assets/img/hidden_2.png');
-    this.addImage('hidden_3', 'assets/img/hidden_3.png');
-    this.addImage('hidden_4', 'assets/img/hidden_4.png');
-    this.addImage('hidden_5', 'assets/img/hidden_5.png');
-    this.addImage('UI_1', 'assets/img/UI_1.png');
-    this.addImage('UI_2', 'assets/img/UI_2.png');
-    this.addImage('UI_3', 'assets/img/UI_3.png');
-    this.addImage('UI_4', 'assets/img/UI_4.png');
-    this.addImage('UI_5', 'assets/img/UI_5.png');
-    this.addImage('UI_btn', 'assets/img/UI_btn.png');
+    this.addImage('bg', 'assets/img/game-assets--background.png');
+    this.addImage('hidden_1', 'assets/img/item-box.png');
+    this.addImage('hidden_2', 'assets/img/item-bunny.png');
+    this.addImage('hidden_3', 'assets/img/item-mug.png');
+    this.addImage('hidden_4', 'assets/img/item-pieter.png');
+    this.addImage('hidden_5', 'assets/img/item-wesley.png');
+    this.addImage('UI_1', 'assets/img/tile-box.png');
+    this.addImage('UI_2', 'assets/img/tile-bunny.png');
+    this.addImage('UI_3', 'assets/img/tile-mug.png');
+    this.addImage('UI_4', 'assets/img/tile-pieter.png');
+    this.addImage('UI_5', 'assets/img/tile-wesley.png');
 };
 
 LoadingState.create = function(){
-    this.game.stage.resize(1334, 750);
     this.game.states.switchState( "PlayState" );
 }
